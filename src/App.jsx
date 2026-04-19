982ee9754d0d8deac5455271abdd982988bbda14
alaa-awad <alaaalmaslh96@gmail.com>
2026-04-19 20:10:11 +0300

    2 commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="animated-container">
+      <span className="name">Eng Alaa</span>
+      <span className="heart">❤️</span>
+      <span className="name">Pharm Aya</span>
+    </div>
+  );
+}
